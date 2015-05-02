d0df90608107571bd1856a60ee32edeea3fa7865
John Marrinan <john_r_marrinan@yahoo.co.uk>
2015-05-02 11:07:25 +0100

    Google maps pin added

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,12 +1,20 @@
-var map;
+var venueMap;
 function initialize() {
   var mapOptions = {
     zoom: 16,
     center: new google.maps.LatLng(51.464,-2.198),
-	mapTypeId: google.maps.MapTypeId.SATELLITE
+    pancontrol: true,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  venueMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  
+  var pinLocation = new google.maps.LatLng(51.4641,-2.198);
+        
+  var startPosition = new google.maps.Marker({    // Create a new marker
+    position: pinLocation,                        // Set its position
+    map: venueMap,                                // Specify the map
+    icon: "./images/img_pin.png"                       // Path to image from HTML
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
